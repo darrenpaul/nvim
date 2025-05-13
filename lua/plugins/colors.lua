d429d7f83ea16b061e1b375f0d63137c40673881
Darren <drobertpaul@gmail.com>
2025-05-13 22:14:35 +0100

    Add Noice Notifications

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,24 +1,15 @@
 local function enable_transparency()
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 end
 
 return {
     {
-	'folke/tokyonight.nvim',
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-	    vim.cmd.colorscheme 'tokyonight'
-	    enable_transparency()
-	end
-    },
-    {
-	'nvim-lualine/lualine.nvim',
-	dependencies = {
-	    'nvim-tree/nvim-web-devicons',
-	},
-	opts = {
-	    theme = 'tokyonight',
-	},
+        'folke/tokyonight.nvim',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            vim.cmd.colorscheme 'tokyonight'
+            enable_transparency()
+        end
     },
 }
